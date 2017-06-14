4d9ad5bab0b5575f4a698e535b69ef8c2d03b136
Andrey Paskal <apaskal@gmail.com>
2017-06-14 19:03:34 +0300

    Simple server for static files

--- app/server.js
+++ app/server.js
@@ -1,7 +1,34 @@
-var http = require('http')
+const http = require('http')
+const url = require('url')
+const fs = require('fs')
+const webPath = __dirname.concat('/../web')
+
 function listener (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'})
-  res.end('<h2>Plain or frameworkless Node.js boilerplate home page</h2>')
+  const purl = url.parse(req.url, true)
+  if (purl.pathname === '/') {
+    purl.pathname = '/index.html'
+  }
+  fs.stat(webPath.concat(purl.pathname), function (err, st) {
+    if (err || !st.isFile()) {
+      res.writeHead(404, {'Content-Type': 'text/plain'})
+      res.end('File Not Found')
+    } else {
+      var extention = purl.pathname.match(/\..+$/)[0]
+      var mime = {
+        '.js': 'text/javascript; charset=UTF-8',
+        '.txt': 'text/plain; charset=UTF-8',
+        '.html': 'text/html; charset=UTF-8',
+        '.png': 'image/png',
+        '.gif': 'image/gif',
+        '.jpg': 'image/jpeg'
+      }
+      res.writeHead(200, {'Content-Type': mime[extention]})
+      fs.readFile(webPath.concat(purl.pathname), function (err, data) {
+        if (err) throw err
+        res.end(data)
+      })
+    }
+  })
 }
 
 const server = http.createServer(listener)
