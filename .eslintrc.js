32e7256a5d6534ba9fcb5009b02fa8a26a6484ae
Andrey Paskal <apaskal@gmail.com>
2017-06-18 18:03:50 +0300

    eslint default config

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // [standard code style config](https://standardjs.com/)
+  'extends': 'standard',
+  'plugins': [
+    'standard',
+    'promise',
+    'json'
+  ],
+  // some exceptions from above standard and additional rules
+  // do comment on you taste
+  'rules': {
+    // comment next line if you like warnings on EOL semicolons
+    'semi': 0
+  }
+};
